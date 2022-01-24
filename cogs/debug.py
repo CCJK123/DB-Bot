35e370acec78236be1727619be57a73700bf7060
notchezstick <66529710+notmeegoreng@users.noreply.github.com>
2022-01-24 13:18:32 +0000

    fix random capital, make debug commmands respond

--- cogs/debug.py
+++ cogs/debug.py
@@ -12,25 +12,25 @@ class DebugCog(discordutils.CogBase):
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def get_keys(self, ctx: discord.ApplicationContext):
-        await ctx.send(await self.bot.database.keys())
+        await ctx.respond(await self.bot.database.keys())
 
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def get_key(self, ctx: discord.ApplicationContext, key: str):
         print(a := await self.bot.database.get(key))
-        await ctx.send(a)
+        await ctx.respond(a)
 
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def del_key(self, ctx: discord.ApplicationContext, key: str):
         await self.bot.database.delete(key)
-        await ctx.send('done')
+        await ctx.respond('done')
 
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def set_key(self, ctx: discord.ApplicationContext, key: str, val: str):
         await self.bot.database.set(key, eval(val))
-        await ctx.send(f'{key} set to {eval(val)}')
+        await ctx.respond(f'{key} set to {eval(val)}')
 
 
 def setup(bot: dbbot.DBBot):
