fd4d91d5d5ad728a66adbd5e5f42e5c73826d7a4
notmeegoreng <terencegoh.y.c@gmail.com>
2022-01-27 16:17:40 +0800

    change option command group setting

--- cogs/options.py
+++ cogs/options.py
@@ -11,7 +11,9 @@ class OptionsCog(discordutils.CogBase):
     def __init__(self, bot: dbbot.DBBot):
         super().__init__(bot, __name__)
 
-    options = commands.SlashCommandGroup('options', "Edit the bot's options", guild_ids=config.guild_ids)
+    options = commands.SlashCommandGroup('options', "Edit the bot's options", guild_ids=config.guild_ids,
+                                         default_permission=False, permissions=[commands.permissions.CommandPermission(
+                                             config.gov_role_id, type=2, permission=True)])
 
     request_options = options.create_subgroup('request', 'Set options for request!')
     request_options.guild_ids = config.guild_ids
