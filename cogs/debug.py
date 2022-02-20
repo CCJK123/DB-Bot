127c4eeb99c3cfadb5c1388763520b91ab6bd575
notchezstick <66529710+notmeegoreng@users.noreply.github.com>
2022-02-20 18:04:26 +0000

    add doc for debug

--- cogs/debug.py
+++ cogs/debug.py
@@ -8,33 +8,38 @@ from utils import discordutils, config
 class DebugCog(discordutils.CogBase):
     def __init__(self, bot: dbbot.DBBot):
         super().__init__(bot, __name__)
-
+    
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def get_keys(self, ctx: discord.ApplicationContext):
+        """Get all the keys in the DB"""
         await ctx.respond(await self.bot.database.keys())
 
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def get_key(self, ctx: discord.ApplicationContext, key: str):
+        """Get the value of a certain key in the DB"""
         print(a := await self.bot.database.get(key))
         await ctx.respond(a)
 
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def del_key(self, ctx: discord.ApplicationContext, key: str):
+        """Delete a key in the DB"""
         await self.bot.database.delete(key)
         await ctx.respond('done')
 
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def set_key(self, ctx: discord.ApplicationContext, key: str, val: str):
+        """Set a key in the DB to a value"""
         await self.bot.database.set(key, eval(val))
         await ctx.respond(f'{key} set to {eval(val)}')
 
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def a(self, ctx: discord.ApplicationContext):
+        """Temp test command"""
         embed = discord.Embed()
         embed.add_field(name=config.resource_emojis['credits'], value=config.resource_emojis['money'])
         await ctx.respond(embed=embed)
@@ -42,6 +47,7 @@ class DebugCog(discordutils.CogBase):
     @commands.command(guild_ids=config.guild_ids, default_permission=False)
     @commands.permissions.has_role(config.gov_role_id, guild_id=config.guild_id)
     async def del_nation(self, ctx: discord.ApplicationContext):
+        """Delete your nation from the registry"""
         cog = self.bot.get_cog('UtilCog')
         await cog.nations[ctx.author.id].delete()
         await ctx.respond('done')
