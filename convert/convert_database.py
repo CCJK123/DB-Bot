42793559f802a8c24f0d8982079bf90f27ef6711
notmeegoreng <notmeegoreng@gmail.com>
2022-06-30 15:34:34 +0800

    update converter to work for latest version (untested)

--- convert/convert_database.py
+++ convert/convert_database.py
@@ -3,15 +3,22 @@ import asyncio
 
 import aiofiles
 
-from utils import databases
+from utils import databases, config, pnwutils
 
 
 async def main():
     async with aiofiles.open('dict.txt') as f:
         data = await f.read()
-    databases.Database()
-    new_db.data = dict(data)
-    await new_db.save()
+    db = databases.PGDatabase(config.database_url)
+    d = dict(data)
+    nations = d['cogs.util.nations']
+    balances = d['cogs.bank.balances']
+    tasks = []
+    for d_id, n_id in nations.items():
+        res = pnwutils.Resources(**balances[d_id])
+        tasks.append(asyncio.create_task(db.execute(
+            f'INSERT INTO users(discord_id, nation_id, balances) VALUES ($1, $2, {res.to_row()})', d_id, n_id)))
+    await asyncio.gather(*tasks)
     print('done')
 
 if __name__ == '__main__':
