7b875545d468f7aeb81a02e8d3808bd2b378d2e7
notmeegoreng <notmeegoreng@gmail.com>
2022-09-23 20:30:49 +0800

    move modal reponse after result await in custom modal

--- utils/financeutils.py
+++ utils/financeutils.py
@@ -405,10 +405,10 @@ class PresetButton(discord.ui.Button[PresetView]):
         discordutils.disable_all(self.view)
         reason_modal = discordutils.single_modal(
             'Why is this request being modified?', 'Modification Reason', discord.TextStyle.paragraph)
-        await discordutils.respond_to_interaction(reason_modal.interaction)
         await asyncio.gather(
             interaction.edit_original_response(view=self.view),
             interaction.response.send_modal(reason_modal))
+        await discordutils.respond_to_interaction(reason_modal.interaction)
         self.view.stop()
         self.view.parent_view.data.resources = self.resources
         self.view.future.set_result(await reason_modal.result())
