e157db438715b8abc5fcac3c5900b3dfcf994cb7
notmeegoreng <notmeegoreng@gmail.com>
2022-10-15 22:15:21 +0800

    slight cleanup

--- cogs/open_slots_detector.py
+++ cogs/open_slots_detector.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 import datetime
 
-from discord.ext import tasks
-
 from utils import discordutils, dbbot
-from utils.queries import find_slots_query
 
 
 class UnnaturalSlotOpenDetectorCog(discordutils.LoopedCogBase):
@@ -14,18 +11,6 @@ class UnnaturalSlotOpenDetectorCog(discordutils.LoopedCogBase):
 
         self.last_data: list[dict] | None = None
         self.last_time: datetime.datetime | None = None
-
-    @tasks.loop(minutes=2)
-    async def task(self):
-        # TODO: figure out enemies
-        data = await find_slots_query.query(self.bot.session, self.enemies)
-        now = datetime.datetime.now()
-        if self.last_data is not None:
-            turn = now.replace(minute=0, second=0, microsecond=0) - datetime.timedelta(hours=now.hour % 2)
-            offset = self.last_time < turn <= now
-
-        self.last_data = data
-        self.last_time = now
 
     def compare(self, pre, nxt):
         return
